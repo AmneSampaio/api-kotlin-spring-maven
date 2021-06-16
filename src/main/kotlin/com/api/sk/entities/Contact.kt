f83a12797b77284c536ea3e470bc7b250fa1b208
AmneSampaio <amnesf@gmail.com>
2021-06-16 15:37:29 -0300

    Criando entidade Contact

--- src/main/kotlin/com/api/sk/entities/Contact.kt
+++ src/main/kotlin/com/api/sk/entities/Contact.kt
@@ -1,7 +1,22 @@
 package com.api.sk.entities
 
-import javax.persistence.Entity
+import com.sun.istack.NotNull
+import javax.persistence.*
+import javax.validation.constraints.Email
+import javax.validation.constraints.Size
 
 @Entity
-class Contact {
+class Contact(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+
+    @NotNull
+    @Size(min = 5, max = 50)
+    var name: String,
+
+    @NotNull
+    @Email
+    var email: String
+)
 }

4ca99be493a29ae6257e033c1fb4383ddd63d1db
AmneSampaio <amnesf@gmail.com>
2021-06-16 15:30:38 -0300

    Criando api Kotlin, com Spring e Maven

---
+++ src/main/kotlin/com/api/sk/entities/Contact.kt
@@ -0,0 +1,7 @@
+package com.api.sk.entities
+
+import javax.persistence.Entity
+
+@Entity
+class Contact {
+}
---
+++ src/test/kotlin/com/api/sk/SkApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.api.sk
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SkApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

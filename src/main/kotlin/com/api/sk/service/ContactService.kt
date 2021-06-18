a3840ba68c1c08c6274ef2619134fdde14c33827
AmneSampaio <amnesf@gmail.com>
2021-06-18 20:06:51 -0300

    Adicionando o ContactForm e adicionando save de telefones

--- src/main/kotlin/com/api/sk/service/ContactService.kt
+++ src/main/kotlin/com/api/sk/service/ContactService.kt
@@ -1,5 +1,6 @@
 package com.api.sk.service
 
+import com.api.sk.dto.ContactForm
 import com.api.sk.entities.Contact
 import com.api.sk.repositories.ContactRepository
 import org.springframework.stereotype.Service
@@ -19,13 +20,14 @@ class ContactService(private val contactRepository: ContactRepository) {
         return contactRepository.save(contact)
     }
 
-    fun atualiza(id: Long, newContact: Contact): Contact {
+    fun atualiza(id: Long, newContact: ContactForm): Contact {
         val contactNoBanco = contactRepository.findById(id).orElseThrow {EntityNotFoundException()}
-        contactNoBanco.apply {
+        val contactAtualizado = contactNoBanco.apply {
             this.name = newContact.name
             this.email = newContact.email
+            this.phones = newContact.phones
         }
-        return contactRepository.save(newContact)
+        return contactRepository.save(contactAtualizado)
     }
 
     fun deleta(id: Long) {

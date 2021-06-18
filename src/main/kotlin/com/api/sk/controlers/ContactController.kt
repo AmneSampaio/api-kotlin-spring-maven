08bd254ec91dc4ed0d0d070b6ed94e58f1c8382a
AmneSampaio <amnesf@gmail.com>
2021-06-18 18:20:28 -0300

    Adicionando o dto de Contact

--- src/main/kotlin/com/api/sk/controlers/ContactController.kt
+++ src/main/kotlin/com/api/sk/controlers/ContactController.kt
@@ -1,8 +1,8 @@
 package com.api.sk.controlers
 
+import com.api.sk.dto.ContactForm
 import com.api.sk.entities.Contact
 import com.api.sk.service.ContactService
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.util.UriComponentsBuilder
@@ -31,8 +31,9 @@ class ContactController(private val contactService: ContactService) {
     }
 
     @PutMapping("/{id}")
-    fun atualiza(@Valid @PathVariable("id") id: Long, @RequestBody newContact: Contact): Contact {
-        return contactService.atualiza(id,newContact)
+    fun atualiza(@Valid @PathVariable("id") id: Long, @RequestBody newContact: ContactForm): ResponseEntity<Contact> {
+        val contactAtualizado = contactService.atualiza(id,newContact)
+        return ResponseEntity.ok(contactAtualizado)
     }
 
     @DeleteMapping("/{id}")

19301b19975cdddf93357b91b74d98808347d065
AmneSampaio <amnesf@gmail.com>
2021-06-24 21:22:40 -0300

    Mudando para var

--- src/main/kotlin/com/api/sk/entities/Contact.kt
+++ src/main/kotlin/com/api/sk/entities/Contact.kt
@@ -14,15 +14,15 @@ data class Contact(
 
     @field:NotEmpty
     @field:Size(min = 5, max = 50, message = "Field name is not valid")
-    val name: String,
+    var name: String,
 
     @field:NotEmpty
     @field:Email(message = "Field email is not valid")
     @field:Column(unique = true)
-    val email: String,
+    var email: String,
 
     @OneToMany(cascade = arrayOf(CascadeType.ALL))
-    val phone: List<Phone>,
+    var phone: List<Phone>,
 
     val data: LocalDateTime = LocalDateTime.now()
 )

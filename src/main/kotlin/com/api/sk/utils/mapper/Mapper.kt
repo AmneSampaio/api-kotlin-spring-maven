cfd54e89ec1c9caa6b01731673a7e78900a42d26
AmneSampaio <amnesf@gmail.com>
2021-07-06 10:24:48 -0300

    Alterando nome da Classe

--- src/main/kotlin/com/api/sk/utils/mapper/Mapper.kt
+++ src/main/kotlin/com/api/sk/utils/mapper/Mapper.kt
@@ -1,10 +1,10 @@
 package com.api.sk.utils.mapper
 
-import com.api.sk.dto.ContactForm
+import com.api.sk.dto.ContactDTO
 
 
 interface Mapper<DTO, Entity> {
 
     fun doEntityToDTO(entity: Entity): DTO
-    fun doDTOToEntity(dto: ContactForm): Entity
+    fun doDTOToEntity(dto: ContactDTO): Entity
 }

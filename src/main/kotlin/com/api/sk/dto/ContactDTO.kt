8b28d480e2b96197f443b35a03fa91f7c8e7bf81
AmneSampaio <amnesf@gmail.com>
2021-07-12 10:43:35 -0300

    Alterando imports

--- src/main/kotlin/com/api/sk/dto/ContactDTO.kt
+++ src/main/kotlin/com/api/sk/dto/ContactDTO.kt
@@ -1,10 +1,6 @@
 package com.api.sk.dto
 
 import com.api.sk.entities.Phone
-import javax.persistence.*
-import javax.validation.constraints.Email
-import javax.validation.constraints.NotEmpty
-import javax.validation.constraints.Size
 
 data class ContactDTO(
 
